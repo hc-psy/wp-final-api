b8fa2766c8b113e317372712621e094bc4843028
hc-psy <austenpsy@gmail.com>
2022-12-30 21:23:40 +0800

    [feature] signup login apis

--- psych/api/psych.py
+++ psych/api/psych.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from psych.db import get_level_1_disorder, get_all_disorders
+from psych.db import get_disorders, create_account, login_check
 
 from flask_cors import CORS
 from psych.api.utils import expect
@@ -12,11 +12,66 @@ psych_api = Blueprint(
 CORS(psych_api)
 
 @psych_api.route('/disorders/', methods=['GET'])
-def api_get_level_1_disorder():
-    response = get_level_1_disorder(disorder_name=request.args.get('lvl_1_disorder'))
+def api_get_disorders():
+    
+    disorder = request.args.get('disorder')
+    response = get_disorders(disorder_name=disorder)
+
     return jsonify(response)
 
-@psych_api.route('/disorders/allDisorders', methods=['GET'])
-def api_get_all_disorders():
-    response = get_all_disorders()
+@psych_api.route('/signup/', methods=['POST'])
+def api_post_signup():
+    
+    username = request.args.get('username')
+    password = request.args.get('password')
+    identity = request.args.get('identity')
+    name = request.args.get('name')
+    email = request.args.get('email')
+    
+    if username and password and identity and name and email:
+        is_success = create_account(username=username, 
+                                    password=password, 
+                                    identity=identity,
+                                    name=name,
+                                    email=email)
+        if is_success:
+            message = 'SUCCESS_ACCOUNT_CREATION'
+            code = 0
+        else:
+            message = 'ACCOUNT_EXIST'
+            code = 1
+    else:
+        message = 'PARAM_ERROR'
+        code = 2
+
+    response = {
+        'message': message,
+        'code': code 
+    }
+
+    return jsonify(response)
+
+@psych_api.route('/login/', methods=['POST'])
+def api_post_login():
+    
+    username = request.args.get('username')
+    password = request.args.get('password')
+    
+    if username and password:
+        code = login_check(username=username, password=password)
+        if code == 0:
+            message = 'SUCCESS_LOGIN'
+        elif code == 1:
+            message = 'WRONG_PASSWORD'
+        elif code == 2:
+            message = 'ACCOUNT_DOESNT_EXIST'
+    else:
+        message = 'PARAM_ERROR'
+        code = 3
+
+    response = {
+        'message': message,
+        'code': code 
+    }
+
     return jsonify(response)
--- psych/db.py
+++ psych/db.py
@@ -3,6 +3,7 @@ import bson
 from flask import current_app, g
 from werkzeug.local import LocalProxy
 from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
 
 from pymongo.errors import DuplicateKeyError, OperationFailure
 from bson.objectid import ObjectId
@@ -25,13 +26,47 @@ def get_db():
 # Use LocalProxy to read the global db instance with just `db`
 db = LocalProxy(get_db)
 
+flask_bcrypt = Bcrypt()
+
+
 def disorders_init():
     db.disorders.delete_many({})
     db.disorders.insert_many(DISORDERS)
     
-def get_level_1_disorder(disorder_name):
-    myquery = { 'level_1_name': disorder_name }
-    return list(db.disorders.find(myquery))
-
-def get_all_disorders():
+def get_disorders(disorder_name):
+    if disorder_name:
+        return list(db.disorders.find({ 'level_1_name': disorder_name }))
+    
     return list(db.disorders.find({}))
+
+def create_account(username, password, identity, name, email):
+    account = db.accounts.find_one({ 'username': username })
+    if account:
+        return False
+    
+    password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+    
+    account = {
+        'username': username,
+        'password': password_hash,
+        'identity': identity,
+        'name': name, 
+        'email': email
+    }
+    
+    db.accounts.insert_one(account)
+    return True
+
+def login_check(username, password):
+    account = db.accounts.find_one({ 'username': username })
+    
+    if not account:
+        return 2
+    
+    if not flask_bcrypt.check_password_hash(account['password'], password):
+        return 1
+    
+    return 0
+    
+    
+
